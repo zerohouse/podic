9c5566b43ece676c1db2a63cf424ebe063c6e392
dev63 <parksungho86@gmail.com>
2016-08-10 18:00:52 +0900

    0.04 minor debug

--- app/scripts/config/version.run.js
+++ app/scripts/config/version.run.js
@@ -1,9 +1,9 @@
-angular.module('Podic').run(function ($ajax, confirm) {
-  $ajax.get('/api/v1/version').then(function (version) {
-    if (version === '0.0.4')
-      return;
-    confirm("새로운 버전이 나왔습니다. 업데이트 받으시겠습니까?", "새로운 버전").then(function () {
-
-    });
-  });
-});
+// angular.module('Podic').run(function ($ajax, confirm) {
+//   $ajax.get('/api/v1/version').then(function (version) {
+//     if (version === '0.0.4')
+//       return;
+//     confirm("새로운 버전이 나왔습니다. 업데이트 받으시겠습니까?", "새로운 버전").then(function () {
+//
+//     });
+//   });
+// });
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+gulp --build
 cordova build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore podic.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
 zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/podic.apk
