20f6e07e96d483cb27d0f6be00c95a06022f7e88
dev63 <parksungho86@gmail.com>
2016-08-14 03:25:12 +0900

    labeling

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 gulp --build
+sleep 1
 cordova build --release android
+sleep 1
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore podic.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
+sleep 1
 zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/podic.apk
+sleep 1
 mv platforms/android/build/outputs/apk/podic.apk ./
